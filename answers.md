48f4382070a8843b7d2cd7f043e8d3ab48916dea
Mats Dale <14minerboe16@gmail.com>
2018-01-18 12:21:24 -0700

    Answer questions and add new responses

---
+++ answers.md
@@ -0,0 +1,16 @@
+# Answers
+
+## My mother and I talked last night.
+"Tell me more about your family."
+## I said no!
+"Why so negative?"
+## The weather is nice.
+"Interesting, tell me more."
+## Do you know my brother?
+"Why so negative?"
+
+Responses are ordered based on the if statements.
+
+# Question
+## Keyword in another word
+The program just looks for the combination of characters not a space on either side to make it, it's own word. This results in unintended responses.
--- src/Magpie2.java
+++ src/Magpie2.java
@@ -32,12 +32,34 @@ public class Magpie2 {
 	public String getResponse(String statement) {
 		String response = "";
 		if (statement.indexOf("no") >= 0) {
-			response = "Why so negative?";
+			response = "Why not?";
 		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+
+		} else if (statement.indexOf("dog") >= 0
+				|| statement.indexOf("cat") >= 0) {
+			response = "Tell me more about your pets.";
+
+		} else if (statement.indexOf("Mr. Padjen") >= 0) {
+			response = "He sounds like a good teacher.";
+
+		} else if (statement.trim().length() == 0) {
+			response = "Say something, please.";
+
+		} else if (statement.indexOf("hi") >= 0
+				|| statement.indexOf("hello") >= 0
+				|| statement.indexOf("hey") >= 0
+				|| statement.indexOf("hola") >= 0) {
+			response = "Hello to you too!";
+
+		} else if (statement.indexOf("bye") >= 0
+				|| statement.indexOf("goodbye") >= 0
+				|| statement.indexOf("cya") >= 0) {
+			response = "Goodbye!";
+
 		} else {
 			response = getRandomResponse();
 		}
@@ -50,7 +72,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,6 +85,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Really?";
+		} else if (whichResponse == 5) {
+			response = "I wonder.";
 		}
 
 		return response;
